81f02adbd932ac0632a5d221aae4a9ee9d4a2054
Merry390 <75833786+Merry390@users.noreply.github.com>
2021-01-06 14:00:31 +0200

    db_migration

---
+++ Asset_Management_App/app/Http/Controllers/AssetsUserController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\assets_user;
+use Illuminate\Http\Request;
+
+class AssetsUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\assets_user  $assets_user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(assets_user $assets_user)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\assets_user  $assets_user
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(assets_user $assets_user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\assets_user  $assets_user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, assets_user $assets_user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\assets_user  $assets_user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(assets_user $assets_user)
+    {
+        //
+    }
+}
---
+++ Asset_Management_App/database/factories/ManufactureFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\manufacture;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ManufactureFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = manufacture::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
